5957d443b19d7b49ccfd6fe122b1a6cf441c8487
Mrigank22 <p.mrigank22@gmail.com>
2025-09-10 16:59:08 +0530

    removed --clear tag

--- runserver.sh
+++ runserver.sh
@@ -2,5 +2,5 @@
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py collectstatic --no-input --clear
+python manage.py collectstatic --no-input
 gunicorn cityelimination.wsgi:application --bind 0.0.0.0:80 --log-level=debug --timeout 180  --workers 4
