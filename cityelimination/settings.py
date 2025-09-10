f77e2f05c5ac1f3a3b4a7389c731f7cad3d70131
Mrigank22 <p.mrigank22@gmail.com>
2025-09-10 16:50:37 +0530

    changed static dir

--- cityelimination/settings.py
+++ cityelimination/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
