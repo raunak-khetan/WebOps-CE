255bb82a585f30fb8e2f6fd1b1a7a25ae3d15c1a
raunak khetan <raunakkhetan470@gmail.com>
2025-09-11 12:52:24 +0530

    added database config

--- cityelimination/settings.py
+++ cityelimination/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = "cityelimination.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if PROD:
+if not PROD:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
     }
