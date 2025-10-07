12505b1c9d650ba4d2f7f663017d558bc3f4c109
Mrigank22 <p.mrigank22@gmail.com>
2025-10-07 23:54:42 +0530

    added audition partners

--- cityelimination/settings.py
+++ cityelimination/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = "cityelimination.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-if not PROD:
+if PROD:
     DATABASES = {
         'default': dj_database_url.config(conn_max_age=600, ssl_require=False)
     }
